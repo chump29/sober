24f3307a8d930490c35653e00f046d2e7b731813
Chris <chris@postfmly.com>
2025-12-31 11:01:48 -0600

    Dynamic copyright domain

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,14 @@ import Dashboard from "./components/dashboard"
 document.getElementById("header")!.innerText =
   `v${import.meta.env.PACKAGE_VERSION}`
 
+const POSTFMLY_US = "postfmly.us"
+const copyright = document.getElementById("copyright") as HTMLAnchorElement
+if (window.location.href.includes(POSTFMLY_US)) {
+  copyright.href = `https://www.${POSTFMLY_US}`
+  copyright.title = `www.${POSTFMLY_US}`
+  copyright.innerText = POSTFMLY_US
+}
+
 if (import.meta.env.DEV) {
   createRoot(document.getElementById("root")!).render(<Dashboard />)
 } else {
