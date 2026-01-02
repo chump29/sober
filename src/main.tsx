31fe27657cf0909925fead91ca016c70f5aac4f4
Chris <chris@postfmly.com>
2026-01-02 10:39:03 -0600

    style: remove header and copyright style: add version to footer chore: update packages

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,13 @@ import { createRoot } from "react-dom/client"
 
 import Dashboard from "./components/dashboard"
 
-document.getElementById("header")!.innerText =
-  `v${import.meta.env.PACKAGE_VERSION}`
-
-document.getElementById("year")!.innerText +=
-  `-${new Date().getFullYear().toString()}`
-
-const POSTFMLY_US = "postfmly.us"
-const copyright = document.getElementById("copyright") as HTMLAnchorElement
-if (window.location.href.includes(POSTFMLY_US)) {
-  copyright.href = `https://www.${POSTFMLY_US}`
-  copyright.title = `www.${POSTFMLY_US}`
-  copyright.innerText = POSTFMLY_US
+function getVersion(version: string) {
+  return version.length ? `v${version}` : "N/A"
 }
+
+document.getElementById("frontend")!.innerText = getVersion(
+  import.meta.env.PACKAGE_VERSION
+)
 
 if (import.meta.env.DEV) {
   createRoot(document.getElementById("root")!).render(<Dashboard />)
