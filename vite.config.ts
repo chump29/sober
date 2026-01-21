7df6f8baa9ae38568a6fb158724e055d85ff1720
Chris <chris@postfmly.com>
2026-01-20 23:06:22 -0600

    build: using simpleHtml plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,29 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import removeAttributes from "rollup-plugin-jsx-remove-attributes"
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
-import versionPlugin from "vite-plugin-package-version"
+import { ViteImageOptimizer as imageOptimizer } from "vite-plugin-image-optimizer"
+import version from "vite-plugin-package-version"
+import simpleHtml from "vite-plugin-simple-html"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [
+    imageOptimizer(),
     react(),
     removeAttributes({
       attributes: ["data-testid"],
       usage: "vite"
     }),
+    simpleHtml({
+      inject: {
+        data: {
+          title: "Sᴏʙᴇᴙ Tᴙᴀᴄᴋᴇᴙ"
+        }
+      },
+      minify: true
+    }),
     tailwindcss(),
-    ViteImageOptimizer(),
-    versionPlugin()
+    version()
   ],
   test: {
     environment: "jsdom",
