346d2438ba827d6a89327512bb616bbd57ae3b0f
admin <chris@postfmly.com>
2026-07-18 12:26:42 -0500

    revert: hiding Stack causes render error in prod

--- frontend/src/components/Display/index.tsx
+++ frontend/src/components/Display/index.tsx
@@ -1,12 +1,4 @@
-import {
-  type ChangeEvent,
-  type EffectCallback,
-  type JSX,
-  type KeyboardEvent,
-  type RefObject,
-  useEffect,
-  useRef
-} from "react"
+import { type ChangeEvent, type EffectCallback, type JSX, type KeyboardEvent, useEffect } from "react"
 
 import { info } from "@postfmly/logger"
 
@@ -138,8 +130,6 @@ const Display = (): JSX.Element => {
 
   const cost: ICost | null = getCost()
   const coin: ICoin | null = getCoin()
-
-  const showCounter: RefObject<boolean> = useRef<boolean>(false)
 
   const nameField = useField<string>({
     initialValue: "",
@@ -586,10 +576,6 @@ const Display = (): JSX.Element => {
     init()
 
     const interval: NodeJS.Timeout = setInterval((): void => {
-      if (!showCounter.current) {
-        showCounter.current = true
-      }
-
       setDisplay(selectedSubstance.date)
     }, ms("1s"))
 
@@ -791,17 +777,15 @@ const Display = (): JSX.Element => {
                   </Tooltip>
                 </Box>
               </Center>
-              {showCounter.current ? (
-                <Stack align="center" c="var(--color-blue)" ff="var(--font-counters)" fw="bold" fz="h1" gap="xs">
-                  <Box>{seconds}</Box>
-                  <Box>{minutes}</Box>
-                  <Box>{hours}</Box>
-                  <Box>{days}</Box>
-                  <Box>{weeks}</Box>
-                  <Box>{months}</Box>
-                  <Box>{years}</Box>
-                </Stack>
-              ) : null}
+              <Stack align="center" c="var(--color-blue)" ff="var(--font-counters)" fw="bold" fz="h1" gap="xs">
+                <Box>{seconds}</Box>
+                <Box>{minutes}</Box>
+                <Box>{hours}</Box>
+                <Box>{days}</Box>
+                <Box>{weeks}</Box>
+                <Box>{months}</Box>
+                <Box>{years}</Box>
+              </Stack>
               {userData?.showCost && cost ? (
                 <Center mt={20}>
                   <Text c="var(--color-red)" fw="bold" inline mr={10} size="xl">
