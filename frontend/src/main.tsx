2c761e33ca1b85a025706321e55a158e09abdbc0
admin <chris@postfmly.com>
2026-09-08 21:49:14 -0500

    chore: update package style: fix CSS selector fix: version display

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,7 @@ import { MantineProvider } from "@mantine/core"
 import { ModalsProvider } from "@mantine/modals"
 import { Notifications } from "@mantine/notifications"
 
-import { info } from "@postfmly/logger"
-import { type Nullable, type Optional } from "@postfmly/types"
+import { type Nullable } from "@postfmly/types"
 
 import { default as httpMethods } from "http-methods-constants"
 import { createRoot } from "react-dom/client"
@@ -11,48 +10,26 @@ import { createRoot } from "react-dom/client"
 import { version } from "../package.json" with { type: "json" }
 import { fetchClient } from "./api/index.ts"
 import { Display } from "./components/Display/index.tsx"
-import { env } from "./env.ts"
 import { findElement, handleError } from "./utils/index.ts"
-import { type IEnv } from "./utils/interfaces/IEnv.ts"
 import { type IFetchClient } from "./utils/interfaces/IFetchClient.ts"
 
-const { SOBER_DEBUG: DEBUG }: IEnv = env
-
-if (DEBUG) {
-  info(`Got UI version: ${version}`)
-}
-
-const getVersion = (v: Optional<string>): string => (v ? `v${v}` : "N/A")
+const getVersion = (v: Nullable<string>): string => (v ? `v${v}` : "N/A")
 
 const frontend: Nullable<HTMLElement> = findElement("#frontend")
 if (frontend) {
-  frontend.innerHTML = `<sup>UI</sup> ${version}`
+  frontend.innerHTML = `<sup>UI</sup> ${getVersion(version)}`
 }
 
 // * NOTE: not using await, don't hold up page render
 fetchClient<string>({
   endpoint: "version",
   method: httpMethods.GET
-} satisfies IFetchClient)
-  .then((data: Nullable<string>): void => {
-    if (data === null) {
-      throw new Error("Could not get API version")
-    }
-
-    const apiVersion: string = getVersion(data)
-
-    if (DEBUG) {
-      info(`Got API version: ${apiVersion}`)
-    }
-
-    const backend: Nullable<HTMLElement> = findElement("#backend")
-    if (backend) {
-      backend.innerHTML = `<sup>API</sup> ${apiVersion}`
-    }
-  })
-  .catch((e: Error): void => {
-    handleError(e)
-  })
+} satisfies IFetchClient).then((data: Nullable<string>): void => {
+  const backend: Nullable<HTMLElement> = findElement("#backend")
+  if (backend) {
+    backend.innerHTML = `<sup>API</sup> ${getVersion(data)}`
+  }
+})
 
 const root: Nullable<HTMLElement> = findElement("#root")
 if (root) {
